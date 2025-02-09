6d954a13e28e0fa7f83e7b7aeb7ead648c6d841a
zmm2025 <zmm2025@gmail.com>
2025-02-09 18:26:16 -0500

    Add Wildcat Joker

--- Bowman.lua
+++ Bowman.lua
@@ -75,3 +75,73 @@ SMODS.Joker {
         end
     end
 }
+
+-- Joker: Wildcat
+SMODS.Joker {
+    key = "wildcat",
+    loc_txt = {
+        name = "Wildcat",
+        text = {
+            "This Joker gains {C:mult}+#2#{}",
+            "Mult every {C:attention}#3#{} {C:inactive}[#4#]{} cards",
+            "played and scored",
+            "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)"
+        }
+    },
+    atlas = "Jokers", pos = { x = 1, y = 0 },
+    config = {
+        extra = {
+            mult = 0,
+            mult_gain = 1,
+            cards_to_score = 7,
+            scored_cards = 7,
+        }
+    },
+    rarity = 2, -- Uncommon
+    cost = 6,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.mult, -- #1#
+                card.ability.extra.mult_gain, -- #2#
+                card.ability.extra.cards_to_score, -- #3#
+                card.ability.extra.scored_cards, -- #4#
+            }
+        }
+    end,
+    
+    calculate = function(self, card, context)
+        -- When a card is scored, decrease scored cards counter
+        if context.individual and context.cardarea == G.play and not context.blueprint then
+            card.ability.extra.scored_cards = card.ability.extra.scored_cards - 1
+            -- If scored cards counter reaches 0, upgrade Joker and reset counter
+            if card.ability.extra.scored_cards <= 0 then
+                card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain
+                card.ability.extra.scored_cards = card.ability.extra.scored_cards + card.ability.extra.cards_to_score
+                return {
+                    card = card,
+                    colour = G.C.MULT,
+                    message = localize("k_upgrade_ex"),
+                }
+            end
+        end
+        
+        -- When Joker is triggered and Mult is >0, add Mult
+        if context.joker_main and card.ability.extra.mult > 0 then
+            return {
+                message = localize {
+                    type = "variable",
+                    key = "a_mult",
+                    vars = {
+                        card.ability.extra.mult,
+                    },
+                },
+                mult_mod = card.ability.extra.mult,
+            }
+        end
+    end
+}
